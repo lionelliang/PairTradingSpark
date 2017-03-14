0de150fb7b4499c76b7c99f4691b322fe47603de
Fuliang <fliguo06@gmail.com>
2017-03-14 22:03:14 +0800

    linear regression with Stochastic Gradient Decent mothod; comparing the efficient of deffrent algorithm

--- checkpairtradingSimultneously.py
+++ checkpairtradingSimultneously.py
@@ -1,3 +1,4 @@
+#coding:utf-8
 import os
 import time
 import operator
@@ -16,25 +17,148 @@ DownloadDir = './stockdata/'
 #date example 2011/10/13
 tudateparser = lambda dates: pd.datetime.strptime(dates, '%Y-%m-%d')
 
+'''
+    linear regression with Stochastic Gradient Decent mothod
+'''
+def linregSGD(x, y, a, b):
+# -------------------------------------------随机梯度下降算法----------------------------------------------------------
+
+    # 两种终止条件
+    loop_max = 10000   # 最大迭代次数(防止死循环)
+    epsilon = 1e-6    
+
+    alpha = 0.001       # 步长(注意取值过大会导致振荡,过小收敛速度变慢)
+    diff = 0.           
+    errorA = a
+    errorB = b
+    count = 0           # 循环次数
+    finish = 0          # 终止标志
+    m = len(x) # 训练数据点数目
+
+    while count < loop_max:
+        count += 1
+
+        # 遍历训练数据集，不断更新权值
+        for i in range(m):  
+            diff = a + b * x[i] - y[i]  # 训练集代入,计算误差值
+
+            # 采用随机梯度下降算法,更新一次权值只使用一组训练数据
+            a = a - alpha * diff
+            b = b - alpha * diff * x[i]
+
+            # ------------------------------终止条件判断-----------------------------------------
+            # 若没终止，则继续读取样本进行处理，如果所有样本都读取完毕了,则循环重新从头开始读取样本进行处理。
+
+        # ----------------------------------终止条件判断-----------------------------------------
+        # 注意：有多种迭代终止条件，和判断语句的位置。终止判断可以放在权值向量更新一次后,也可以放在更新m次后。
+        if ((a-errorA)*(a-errorA) + (b-errorB)*(b-errorB)) < epsilon:     # 终止条件：前后两次计算出的权向量的绝对误差充分小  
+            finish = 1
+            break
+        else:
+            errorA = a
+            errorB = b
+    print 'loop count = %d' % count,  '\tweight:[%f, %f]' % (a, b)
+    return a, b
+
+def adfuller_check_sgd(closeprice_of_1, closeprice_of_2, a, b):
+
+    if len(closeprice_of_1) != 0 and len(closeprice_of_2) != 0:
+        alpha, beta = linregSGD(x=closeprice_of_1, y=closeprice_of_2, a=a, b=b)
+
+        spread = closeprice_of_2 - closeprice_of_1*beta - alpha
+        adfstat, pvalue, usedlag, nobs, critvalues, icbest = sts.adfuller(x=spread)
+
+        return adfstat < critvalues['5%']
+'''        
+        print adfstat
+        for(k, v) in critvalues.items():
+            print k, v
+'''
+
 def adfuller_check_smols(closeprice_of_1, closeprice_of_2):
 
     if len(closeprice_of_1) != 0 and len(closeprice_of_2) != 0:
         X = sm.add_constant(closeprice_of_1)
         model = sm.OLS(endog=closeprice_of_2, exog=X)
         result = model.fit()
-#        print result.summary()
+        #print result.summary()
+        print result.params
         spread = result.resid
-        stat = sts.adfuller(x=spread)
-        adf = stat[0]
-        pvalue = stat[1]
-        critical_values = stat[4]
+        adfstat, pvalue, usedlag, nobs, critvalues, icbest = sts.adfuller(x=spread)
 
-        return adf < critical_values['10%']
-#       for(k, v) in critical_values.items():
-#           print k, v
-#        spread2 = closeprice_of_2 - closeprice_of_1*result.params.closel
-#        sta2 = sts.adfuller(spread, 1)
-#        print sta2
+        return adfstat < critvalues['5%']
+'''
+        print adfstat
+        for(k, v) in critvalues.items():
+            print k, v
+'''
+'''
+        spread2 = closeprice_of_2 - closeprice_of_1*result.params.closel
+        sta2 = sts.adfuller(spread, 1)
+        print sta2
+'''
+def compare_algorithm(code1, code2, start_date = '2013-10-10', end_date = '2014-09-30'):
+    
+    closeprice_of_1, closeprice_of_2 = load_process(code1, code2, start_date, end_date)
+
+    time1 = time.time()
+    result = adfuller_check_smols(closeprice_of_1, closeprice_of_2)
+    time2 = time.time()
+    print "smols running time(s): ", time2-time1
+    
+    time3 = time.time()
+    a = -1
+    b = -1
+    result = adfuller_check_sgd(closeprice_of_1, closeprice_of_2, a, b)
+    time4 = time.time()
+    print "sgd running time(s): ", time4-time3
+
+    time5 = time.time()
+    a = 0
+    b = 0
+    result = adfuller_check_sgd(closeprice_of_1, closeprice_of_2, a, b)
+    time6 = time.time()
+    print "sgdmiddle running time(s): ", time6-time5
+
+def adfuller_check_price_sgd(code1, code2, start_date = '2011-10-10', end_date = '2014-09-30', linrreg="SMOLS"):
+
+    closeprice_of_1, closeprice_of_2 = load_process(code1, code2, start_date, end_date)
+
+    if linrreg == "SMOLS" :
+        result = adfuller_check_smols(closeprice_of_1, closeprice_of_2)
+    elif linrreg == "SGD" :
+        a = 0
+        b = 0
+        result = adfuller_check_sgd(closeprice_of_1, closeprice_of_2, a, b)
+    elif linrreg == "SGDMiddle" :
+        a = 0
+        b = 0
+        result = adfuller_check_sgd(closeprice_of_1, closeprice_of_2, a, b)
+    else :
+        result = ""
+    return result
+
+def load_process(code1, code2, start_date, end_date):
+    m = str(code1)
+    n = str(code2)
+    file1 = DownloadDir + "h_kline_" + code1 + ".csv"
+    file2 = DownloadDir + "h_kline_" + code2 + ".csv"
+    if not os.path.exists(file1) or not os.path.exists(file1):
+        return
+
+    kline1 = pd.read_csv(file1, parse_dates=['date'], index_col='date', date_parser=tudateparser)
+    kline2 = pd.read_csv(file2, parse_dates=['date'], index_col='date', date_parser=tudateparser)
+    #print kline1.head()
+    price_of_1 = kline1[end_date:start_date]
+    price_of_2 = kline2[end_date:start_date]
+
+    # regroup quotation according to date index
+    combination = price_of_1.join(price_of_2, how='inner', lsuffix='l', rsuffix='r')
+    combination.dropna()
+
+    closeprice_of_1 = combination['closel'].reset_index(drop=True)
+    closeprice_of_2 = combination['closer'].reset_index(drop=True)
+    return closeprice_of_1, closeprice_of_2
 
 def simulate_check_5days(close1, close2):
     
@@ -102,9 +226,13 @@ def check_all_dir():
 def main():
     time1 = time.time()
 
-    adfuller_check_price("601002", "600815")
+    #adfuller_check_price("601002", "600815")
     # chedk all stock pairing in list book
     #check_all_dir()
+
+    #adfuller_check_price_sgd("601002", "600815",start_date = '2013-10-10', 
+    #            end_date = '2014-09-30', linrreg="SMOLS")           #"SGD")
+    compare_algorithm("601002", "600815",start_date = '2013-10-10', end_date = '2014-09-30')
 
     time2 = time.time()
     print "running time(s): ", time2-time1
---
+++ sgd.py
@@ -0,0 +1,182 @@
+#coding:utf-8
+import numpy as np
+import time
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+from scipy import stats
+
+def SGD(x, target_data, a, b):
+# -------------------------------------------随机梯度下降算法----------------------------------------------------------
+
+    # 两种终止条件
+    loop_max = 1000   # 最大迭代次数(防止死循环)
+    epsilon = 1e-6    
+
+    alpha = 0.001       # 步长(注意取值过大会导致振荡,过小收敛速度变慢)
+    diff = 0.           
+    errorA = a
+    errorB = b
+    count = 0           # 循环次数
+    finish = 0          # 终止标志
+    m = len(x) # 训练数据点数目
+
+    while count < loop_max:
+        count += 1
+
+        # 遍历训练数据集，不断更新权值
+        for i in range(m):  
+            diff = a + b * x[i] - target_data[i]  # 训练集代入,计算误差值
+
+            # 采用随机梯度下降算法,更新一次权值只使用一组训练数据
+            a = a - alpha * diff
+            b = b - alpha * diff * x[i]
+
+            # ------------------------------终止条件判断-----------------------------------------
+            # 若没终止，则继续读取样本进行处理，如果所有样本都读取完毕了,则循环重新从头开始读取样本进行处理。
+
+        # ----------------------------------终止条件判断-----------------------------------------
+        # 注意：有多种迭代终止条件，和判断语句的位置。终止判断可以放在权值向量更新一次后,也可以放在更新m次后。
+        if ((a-errorA)*(a-errorA) + (b-errorB)*(b-errorB)) < epsilon:     # 终止条件：前后两次计算出的权向量的绝对误差充分小  
+            finish = 1
+            break
+        else:
+            errorA = a
+            errorB = b
+    print 'loop count = %d' % count,  '\tweight:[%f, %f]' % (a, b)
+    return count, a, b
+
+def SGD2(x, target_data, w):
+# -------------------------------------------随机梯度下降算法----------------------------------------------------------
+
+    # 两种终止条件
+    loop_max = 1000   # 最大迭代次数(防止死循环)
+    epsilon = 1e-3     
+
+    alpha = 0.001       # 步长(注意取值过大会导致振荡,过小收敛速度变慢)
+    diff = 0.           
+    error = np.zeros(2)
+    count = 0           # 循环次数
+    finish = 0          # 终止标志
+    m = len(x) # 训练数据点数目
+    x0 = np.full(m, 1.0)                 
+    input_data = np.vstack([x0, x]).T               # 将偏置b作为权向量的第一个分量
+
+    while count < loop_max:
+        count += 1
+
+        # 遍历训练数据集，不断更新权值
+        for i in range(m):  
+            diff = np.dot(w, input_data[i]) - target_data[i]  # 训练集代入,计算误差值
+
+            # 采用随机梯度下降算法,更新一次权值只使用一组训练数据
+            w = w - alpha * diff * input_data[i]
+
+            # ------------------------------终止条件判断-----------------------------------------
+            # 若没终止，则继续读取样本进行处理，如果所有样本都读取完毕了,则循环重新从头开始读取样本进行处理。
+
+        # ----------------------------------终止条件判断-----------------------------------------
+        # 注意：有多种迭代终止条件，和判断语句的位置。终止判断可以放在权值向量更新一次后,也可以放在更新m次后。
+        if np.linalg.norm(w - error) < epsilon:     # 终止条件：前后两次计算出的权向量的绝对误差充分小  
+            finish = 1
+            break
+        else:
+            error = w
+    print 'loop count = %d' % count,  '\tw:[%f, %f]' % (w[0], w[1])
+    return count, w
+
+'''
+def SGD(input_data, target_data):
+# -----------------------------------------------梯度下降法-----------------------------------------------------------
+
+    # 两种终止条件
+    loop_max = 10000   # 最大迭代次数(防止死循环)
+    epsilon = 1e-3     
+
+    # 初始化权值
+    np.random.seed(0)
+    w = np.random.randn(2)
+    #w = np.zeros(2)
+
+    alpha = 0.001      # 步长(注意取值过大会导致振荡,过小收敛速度变慢)
+    diff = 0.           
+    error = np.zeros(2) 
+    count = 0          # 循环次数
+    finish = 0         # 终止标志
+    time1 = time.time()
+
+    while count < loop_max:
+        count += 1
+
+        # 标准梯度下降是在权值更新前对所有样例汇总误差，而随机梯度下降的权值是通过考查某个训练样例来更新的
+        # 在标准梯度下降中，权值更新的每一步对多个样例求和，需要更多的计算
+        sum_m = np.zeros(2)
+        for i in range(m):
+            dif = (np.dot(w, input_data[i]) - target_data[i]) * input_data[i]
+            sum_m = sum_m + dif     # 当alpha取值过大时,sum_m会在迭代过程中会溢出
+
+        w = w - alpha * sum_m       # 注意步长alpha的取值,过大会导致振荡
+        #w = w - 0.005 * sum_m      # alpha取0.005时产生振荡,需要将alpha调小
+        
+        # 判断是否已收敛
+        if np.linalg.norm(w - error) < epsilon:
+            finish = 1
+            break
+        else:
+            error = w
+    print 'loop count = %d' % count,  '\tw:[%f, %f]' % (w[0], w[1])
+'''
+
+## Main functionality
+def main():
+    # 构造训练数据
+    x = np.arange(0., 10., 0.2)
+    m = len(x)                                      # 训练数据点数目
+    print "m=%d" % m
+    target_data = 2 * x + 5 + np.random.randn(m)
+    # 初始化权值
+    #np.random.seed(0)
+    #w = np.random.randn(2)
+    #w = np.zeros(2)             # 7 times recurtion
+    a = 4.673431                 # 104 times recurtion
+    b = 2.048986
+    #w = np.array([4.881609, 2.016084])    # 4 times recursion
+
+    #SGD2(x, target_data, w)
+    # loop from middle stat
+    time1 = time.time()
+    SGD(x, target_data, a, b)
+    time2 = time.time()
+    print "optimised sgd loop time(s): ", time2-time1
+
+    # full loop with sgd
+    a2 = 0                      # 324 times recurtion
+    b2 = 0
+    time7 = time.time()
+    SGD(x, target_data, a2, b2)
+    time8 = time.time()
+    print "sgd loop time(s): ", time8-time7
+
+    # check with scipy linear regression 
+    time3 = time.time()
+    slope, intercept, r_value, p_value, slope_std_error = stats.linregress(x, target_data)
+    time4 = time.time()
+    print 'intercept = %s slope = %s' %(intercept, slope)
+    print "linregress time(s): ", time4-time3
+
+    time5 = time.time()
+    X = sm.add_constant(x)
+    model = sm.OLS(endog=target_data, exog=X)
+    result = model.fit()
+    time6 = time.time()
+    print "params", result.params
+    print "OLS time(s): ", time6-time5
+
+    '''
+    plt.plot(x, target_data, 'k+')
+    plt.plot(x, w[1] * x + w[0], 'r')
+    plt.show()
+    '''
+
+if __name__ == "__main__":
+    # Execute Main functionality
+    main()
