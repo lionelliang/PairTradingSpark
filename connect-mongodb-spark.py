85fa18f81027e08ec98d3671c9d996f9b522e160
Fuliang <fliguo06@gmail.com>
2017-04-18 15:11:01 +0800

    change the mongo db connector to Stratio/Spark-MongoDB

--- connect-mongodb-spark.py
+++ connect-mongodb-spark.py
@@ -1,5 +1,6 @@
 #coding:utf-8
 from pyspark import SparkContext, SparkConf
+from pyspark.sql import SparkSession
 
 import pymongo_spark
 # Important: activate pymongo_spark.
@@ -9,7 +10,8 @@ pymongo_spark.activate()
 def main():
     conf = SparkConf().setAppName("pyspark test")
     sc = SparkContext(conf=conf)
-
+'''
+    # pymongo_spark version
     # Create an RDD backed by the MongoDB collection.
     # This RDD *does not* contain key/value pairs, just documents.
     # If you want key/value pairs, use the mongoPairRDD method instead.
@@ -17,11 +19,24 @@ def main():
     print rdd.first()
 
     # Save this RDD back to MongoDB as a different collection.
-    rdd.saveToMongoDB('mongodb://localhost:27017/stock.linrreg')
+    rdd.saveToMongoDB('mongodb://localhost:27017/stock.linrregother')
 
     # You can also read and write BSON:
     #bson_rdd = sc.BSONFileRDD('/path/to/file.bson')
     #bson_rdd.saveToBSON('/path/to/bson/output')
+'''
+'''
+#spark-mongo version
+spark = SparkSession.builder.getOrCreate()
+spark.sql("CREATE TEMPORARY VIEW linrreg_table USING com.stratio.datasource.mongodb OPTIONS (host 'localhost:27017', database 'stock', collection 'linrreg')")
+spark.sql("SELECT * FROM linrreg_table").collect()
+'''
+df = sparkSession.read.format('com.stratio.datasource.mongodb').options(host='localhost:27017', database='stock', collection='linrreg').load()
+df.collect()
+
+df.write.format("com.stratio.datasource.mongodb").mode('overwrite').options(host='localhost:27017', database='stock', collection='linrregother').save()
+dfView = sparkSession.read.format('com.stratio.datasource.mongodb').options(host='localhost:27017', database='stock', collection='linrregother').load()
+dfView.show()
 
 if __name__ == '__main__':
     main()
